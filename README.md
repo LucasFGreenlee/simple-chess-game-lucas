ed7ec16013d88ddfd65a0fb2c93f8be0ce24150a
LucasFGreenlee <lucasfgreenlee@gmail.com>
2023-04-06 19:02:38 -0700

    feat : added notes to work on tonight / tomorrow

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # simple-chess-game-lucas
+
+base class Piece
+    render
+    movement
+    alive = true
+    move to graveyard
+    position
+    img src = "?"
+    movement rule
+
+
+newPawn = new Piece (position, img-src)
+    
+class Bishop extends Piece
+
+class Knight extends Piece
+
+class Rook extends Piece
+
+class Queen extends Piece
+
+class King extends Piece
--- app.js
+++ app.js
@@ -17,6 +17,7 @@
 //}
 
 let board = document.getElementById("ChessBoard");
+//let bpawn1 = 
 let map = [
   3, 5, 4, 2, 1, 4, 5, 3,
   6, 6, 6, 6, 6, 6, 6, 6,
@@ -28,10 +29,23 @@ let map = [
   9,11,10, 8, 7,10,11, 9,
 ];
 
-for (let i=0; i< 64; i++){
+for (let i=0; i< map.length; i++){
   let tile = document.createElement("div");
+  // setting tiles background color
   tile.style.backgroundColor = parseInt((i / 8) + i) % 2 == 0 ? 'white' : 'lightgray';
-  tile.innerHTML = !map[i] ? "" : "&#"+ (9811+map[i]) +";";
+  // place pieces on the board
+  // tile.innerHTML = !map[i] ? "" : "&#"+ (9811+map[i]) +";";
+switch(map[i]) {
+  case 0:
+    break;
+  case 6: // Black pawns
+    // newPawn = new Piece (position = i, "&#"+ 9817 +";")
+    break;
+    // add all pieces as case statements but dont delete default
+  default:
+    console.log('Try again next time!');
+    break;
+}
   board.appendChild(tile);
 }
 
