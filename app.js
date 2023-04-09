9109254ac292a9bc197ea38017e407f759675e6c
LucasFGreenlee <lucasfgreenlee@gmail.com>
2023-04-08 17:29:25 -0700

    feat: tried to add chess pieces to board

--- app.js
+++ app.js
@@ -2,50 +2,6 @@ const ctx = game.getContext('2d');
 game.setAttribute('height', getComputedStyle(game)['height']);
 game.setAttribute('width', getComputedStyle(game)['width']);
 
-var center = document.createElement('center');
-  
-// Create a table element
-var ChessTable = document.createElement('table');
-for (var i = 0; i < 8; i++) {
-  
-    // Create a row
-    var tr = document.createElement('tr');
-    for (var j = 0; j < 8; j++) {
-  
-        // Create a cell
-        var td = document.createElement('td');
-  
-        // If the sum of cell coordinates is even
-        // then color the cell white
-        if ((i + j) % 2 == 0) {
-  
-            // Create a class attribute for all white cells
-            td.setAttribute('class', 'cell whitecell');
-            tr.appendChild(td);
-        }
-  
-        // If the sum of cell coordinates is odd then
-        // color the cell black
-        else {
-  
-            // Create a class attribute for all black cells
-            td.setAttribute('class', 'cell blackcell');
-  
-            // Append the cell to its row
-            tr.appendChild(td);
-        }
-    }
-  
-    // Append the row
-    ChessTable.appendChild(tr);
-}
-center.appendChild(ChessTable);
-  
-// Modifying table attribute properties
-ChessTable.setAttribute('cellspacing', '0');
-ChessTable.setAttribute('width', '640px');
-document.body.appendChild(center);
-
 let b_pawn1;
 let b_pawn2;
 let b_pawn3;
@@ -62,14 +18,14 @@ let b_knight1;
 let b_knight2;
 let b_rook1;
 let b_rook2;
-let w_pawn1 ;
-let w_pawn2 ;
-let w_pawn3 ;
-let w_pawn4 ;
-let w_pawn5 ;
-let w_pawn6 ;
-let w_pawn7 ;
-let w_pawn8 ;
+let w_pawn1;
+let w_pawn2;
+let w_pawn3;
+let w_pawn4;
+let w_pawn5;
+let w_pawn6;
+let w_pawn7;
+let w_pawn8;
 let w_king;
 let w_queen;
 let w_bishop1;
@@ -80,7 +36,7 @@ let w_rook1;
 let w_rook2;
 
 class chessPiece {
-  constructor(x, y, image, type, team) {
+  constructor(x, y, image, type, team,) {
     this.x = x;
     this.y = y;
     this.image = image;
@@ -96,38 +52,100 @@ class chessPiece {
   }
 }
 
-let pieces = [
-  new chessPiece('x', 'y', "&#" + 9823 + ";", 'Pawn', 'Black'),
-  new chessPiece('x', 'y', "&#" + 9823 + ";", 'Pawn', 'Black'),
-  new chessPiece('x', 'y', "&#" + 9823 + ";", 'Pawn', 'Black'),
-  new chessPiece('x', 'y', "&#" + 9823 + ";", 'Pawn', 'Black'),
-  new chessPiece('x', 'y', "&#" + 9823 + ";", 'Pawn', 'Black'),
-  new chessPiece('x', 'y', "&#" + 9823 + ";", 'Pawn', 'Black'),
-  new chessPiece('x', 'y', "&#" + 9823 + ";", 'Pawn', 'Black'),
-  new chessPiece('x', 'y', "&#" + 9823 + ";", 'Pawn', 'Black'),
-  new chessPiece('x', 'y', "&#" + 9818 + ";", 'King', 'Black'),
-  new chessPiece('x', 'y', "&#" + 9819 + ";", 'Queen', 'Black'),
-  new chessPiece('x', 'y', "&#" + 9821 + ";", 'Bishop', 'Black'),
-  new chessPiece('x', 'y', "&#" + 9821 + ";", 'Bishop', 'Black'),
-  new chessPiece('x', 'y', "&#" + 9822 + ";", 'Knight', 'Black'),
-  new chessPiece('x', 'y', "&#" + 9822 + ";", 'Knight', 'Black'),
-  new chessPiece('x', 'y', "&#" + 9820 + ";", 'Rook', 'Black'),
-  new chessPiece('x', 'y', "&#" + 9820 + ";", 'Rook', 'Black'),
-  new chessPiece('x', 'y', "&#" + 9817 + ";", 'Pawn', 'White'),
-  new chessPiece('x', 'y', "&#" + 9817 + ";", 'Pawn', 'White'),
-  new chessPiece('x', 'y', "&#" + 9817 + ";", 'Pawn', 'White'),
-  new chessPiece('x', 'y', "&#" + 9817 + ";", 'Pawn', 'White'),
-  new chessPiece('x', 'y', "&#" + 9817 + ";", 'Pawn', 'White'),
-  new chessPiece('x', 'y', "&#" + 9817 + ";", 'Pawn', 'White'),
-  new chessPiece('x', 'y', "&#" + 9817 + ";", 'Pawn', 'White'),
-  new chessPiece('x', 'y', "&#" + 9817 + ";", 'Pawn', 'White'),
-  new chessPiece('x', 'y', "&#" + 9812 + ";", 'King', 'White'),
-  new chessPiece('x', 'y', "&#" + 9813 + ";", 'Queen', 'White'),
-  new chessPiece('x', 'y', "&#" + 9815 + ";", 'Bishop', 'White'),
-  new chessPiece('x', 'y', "&#" + 9815 + ";", 'Bishop', 'White'),
-  new chessPiece('x', 'y', "&#" + 9816 + ";", 'Knight', 'White'),
-  new chessPiece('x', 'y', "&#" + 9816 + ";", 'Knight', 'White'),
-  new chessPiece('x', 'y', "&#" + 9814 + ";", 'Rook', 'White'),
-  new chessPiece('x', 'y', "&#" + 9814 + ";", 'Rook', 'White')]
+var center = document.createElement('center');
 
+// Create a table element
+var ChessTable = document.createElement('table');
+for (var i = 0; i < 8; i++) {
+  var tr = document.createElement('tr');
+  for (var j = 0; j < 8; j++) {
+    var td = document.createElement('td');
+    if ((i + j) % 2 == 0) {
+      td.setAttribute('class', 'cell whitecell');
+      tr.appendChild(td);
+    }
+    else {
+      td.setAttribute('class', 'cell blackcell');
+      tr.appendChild(td);
+    }
+  }
+  ChessTable.appendChild(tr);
+}
+center.appendChild(ChessTable);
 
+ChessTable.setAttribute('cellspacing', '0');
+ChessTable.setAttribute('width', '640px');
+document.body.appendChild(center);
+
+window.addEventListener('DOMContentLoaded', function () {
+
+  b_pawn1 = new chessPiece(40, 120, "&#" + 9823 + ";", 'Pawn', 'Black')
+  b_pawn2 = new chessPiece(120, 120, "&#" + 9823 + ";", 'Pawn', 'Black')
+  b_pawn3 = new chessPiece(200, 120, "&#" + 9823 + ";", 'Pawn', 'Black')
+  b_pawn4 = new chessPiece(280, 120, "&#" + 9823 + ";", 'Pawn', 'Black')
+  b_pawn5 = new chessPiece(360, 120, "&#" + 9823 + ";", 'Pawn', 'Black')
+  b_pawn6 = new chessPiece(440, 120, "&#" + 9823 + ";", 'Pawn', 'Black')
+  b_pawn7 = new chessPiece(520, 120, "&#" + 9823 + ";", 'Pawn', 'Black')
+  b_pawn8 = new chessPiece(600, 120, "&#" + 9823 + ";", 'Pawn', 'Black')
+  b_king = new chessPiece(280, 60, "&#" + 9818 + ";", 'King', 'Black')
+  b_queen = new chessPiece(360, 60, "&#" + 9819 + ";", 'Queen', 'Black')
+  b_bishop1 = new chessPiece(200, 60, "&#" + 9821 + ";", 'Bishop', 'Black')
+  b_bishop2 = new chessPiece(440, 60, "&#" + 9821 + ";", 'Bishop', 'Black')
+  b_knight1 = new chessPiece(120, 60, "&#" + 9822 + ";", 'Knight', 'Black')
+  b_knight2 = new chessPiece(520, 60, "&#" + 9822 + ";", 'Knight', 'Black')
+  b_rook1 = new chessPiece(40, 60, "&#" + 9820 + ";", 'Rook', 'Black')
+  b_rook2 = new chessPiece(600, 60, "&#" + 9820 + ";", 'Rook', 'Black')
+  w_pawn1 = new chessPiece(40, 520, "&#" + 9817 + ";", 'Pawn', 'White')
+  w_pawn2 = new chessPiece(120, 520, "&#" + 9817 + ";", 'Pawn', 'White')
+  w_pawn3 = new chessPiece(200, 520, "&#" + 9817 + ";", 'Pawn', 'White')
+  w_pawn4 = new chessPiece(280, 520, "&#" + 9817 + ";", 'Pawn', 'White')
+  w_pawn5 = new chessPiece(360, 520, "&#" + 9817 + ";", 'Pawn', 'White')
+  w_pawn6 = new chessPiece(440, 520, "&#" + 9817 + ";", 'Pawn', 'White')
+  w_pawn7 = new chessPiece(520, 520, "&#" + 9817 + ";", 'Pawn', 'White')
+  w_pawn8 = new chessPiece(600, 520, "&#" + 9817 + ";", 'Pawn', 'White')
+  w_king = new chessPiece(280, 600, "&#" + 9812 + ";", 'King', 'White')
+  w_queen = new chessPiece(360, 600, "&#" + 9813 + ";", 'Queen', 'White')
+  w_bishop1 = new chessPiece(200, 600, "&#" + 9815 + ";", 'Bishop', 'White')
+  w_bishop2 = new chessPiece(440, 600, "&#" + 9815 + ";", 'Bishop', 'White')
+  w_knight1 = new chessPiece(120, 600, "&#" + 9816 + ";", 'Knight', 'White')
+  w_knight2 = new chessPiece(520, 600, "&#" + 9816 + ";", 'Knight', 'White')
+  w_rook1 = new chessPiece(40, 600, "&#" + 9814 + ";", 'Rook', 'White')
+  w_rook2 = new chessPiece(600, 600, "&#" + 9814 + ";", 'Rook', 'White')
+
+  const runGame = setInterval(gameLoop, 10);
+});
+
+function gameLoop() {
+  b_pawn1.render()
+  b_pawn2.render()
+  b_pawn3.render()
+  b_pawn4.render()
+  b_pawn5.render()
+  b_pawn6.render()
+  b_pawn7.render()
+  b_pawn8.render()
+  b_king.render()
+  b_queen.render()
+  b_bishop1.render()
+  b_bishop2.render()
+  b_knight1.render()
+  b_knight2.render()
+  b_rook1.render()
+  b_rook2.render()
+  w_pawn1.render()
+  w_pawn2.render()
+  w_pawn3.render()
+  w_pawn4.render()
+  w_pawn5.render()
+  w_pawn6.render()
+  w_pawn7.render()
+  w_pawn8.render()
+  w_king.render()
+  w_queen.render()
+  w_bishop1.render()
+  w_bishop2.render()
+  w_knight1.render()
+  w_knight2.render()
+  w_rook1.render()
+  w_rook2.render()
+}
